7bf6fd4c4c7e77461b4d664bb9e56d3d41bfcc56
sglueck <sglueck1@gmail.com>
2012-09-29 12:56:32 -0400

    first commit

---
+++ Homer/Test/src/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(args.length+" arguments found");
+		//for-each loop = we'll come back to this (Note: It doesn't say "each")
+		for(String arg:args) {
+			System.out.println(arg);
+		}
+
+	}
+
+}
